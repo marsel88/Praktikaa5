b827803d556bc7e8811fb451770c3fa7c105ecc9
Марсель Баринов <barinov.marsel1111@gmail.com>
2022-03-23 13:18:17 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp2
+{
+    public class Product
+    {
+        public Product(string name, string manufacturer, decimal price, int amount)
+        {
+            Name = name;
+            Manufacturer = manufacturer;
+            Price = price;
+            Amount = amount;
+
+            Description = "Non Description";
+        }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        private string Manufacturer { get; set; }
+        private decimal Price { get; set; }
+        private bool Activity { get; set; }
+        private int Amount { get; set; }
+        public void AddDescription(string s)
+        {
+            Description = s;
+        }
+        public override string ToString()
+        {
+            return $"{Name}:\n\t{Description}\n\t{Manufacturer}\n\t{Price}$\n\t{Activity} {Amount}";
+        }
+    }
+}
---
+++ WpfApp2/ProductHolder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp2
+{
+    public class ProductHolder
+    {
+        public List<Product> Products { get; set; }
+        
+    public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
+    public void RemoveProduct(Product product)
+        {
+            Products.Remove(product);
+        }
+    public void SortForPriceUp()
+        {
+            //code
+        }
+    public void SortForPriceDown()
+        {
+            //code
+        }
+    }
+}
